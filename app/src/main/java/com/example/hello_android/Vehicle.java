c8c7579937f2620a5ce444ee165495862da09fb5
brett <brett@criticalanalysis.com.au>
2019-04-25 20:48:13 +1000

    Edited Vehicle Class

--- app/src/main/java/com/example/hello_android/Vehicle.java
+++ app/src/main/java/com/example/hello_android/Vehicle.java
@@ -1,4 +1,11 @@
 package com.example.hello_android;
 
+
+import java.util.Date;
+
 class Vehicle {
+    private VehicleOwner owner;
+    private String registration;
+    private Date serviceDate;
+    private int odometer;
 }
