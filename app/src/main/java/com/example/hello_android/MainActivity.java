5926ef46c5c8f8586b91cefc62e9d4b88b39f2f4
brett <brett@criticalanalysis.com.au>
2019-04-27 18:22:46 +1000

    Wired up MainActivity Class. Changed UI from LinearLayout to ScrollLayout

--- app/src/main/java/com/example/hello_android/MainActivity.java
+++ app/src/main/java/com/example/hello_android/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         mNewVehicleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(MainActivity.this,AddNewVehicle.class);
+                startActivity(intent);
             }
         });
     }
