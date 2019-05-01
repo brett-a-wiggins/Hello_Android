5902816fd869843a02666d556389d62972bf48cd
anvilRunSlay <s3766659@student.rmit.edu.au>
2019-05-01 20:11:04 +0800

    Edited transaction classes and added constructors etc and addTransaction methods

--- app/src/main/java/com/example/hello_android/ServiceTransaction.java
+++ app/src/main/java/com/example/hello_android/ServiceTransaction.java
@@ -1,4 +1,15 @@
 package com.example.hello_android;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class ServiceTransaction extends Transaction {
+    private BigDecimal partCost;
+    private BigDecimal laborCost;
+
+    public ServiceTransaction(Date transactionDate, BigDecimal partCost, BigDecimal laborCost, BigDecimal totalCost) {
+        super(transactionDate, totalCost);
+        this.partCost = partCost;
+        this.laborCost = laborCost;
+    }
 }
