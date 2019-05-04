5c08f33c5be299ac2e05b796ade1357160441828
anvilRunSlay <s3766659@student.rmit.edu.au>
2019-05-04 14:58:57 +0800

    edited existing user activity

--- app/src/main/java/com/example/hello_android/ExistingUserActivity.java
+++ app/src/main/java/com/example/hello_android/ExistingUserActivity.java
@@ -1,6 +1,8 @@
 package com.example.hello_android;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
 
 public class ExistingUserActivity extends AppCompatActivity {
+    private Spinner userSpinner;
 }
