69fad28f74fb850860b22d9d380f49550d1814e7
anvilRunSlay <s3766659@student.rmit.edu.au>
2019-05-07 17:38:55 +0800

    added export data, fuel log menu, fuel statistics menu, receipt storage menu, service log menu, service statistics menu activities and their layouts

---
+++ app/src/main/java/com/example/hello_android/FuelLogMenuActivity.java
@@ -0,0 +1,8 @@
+package com.example.hello_android;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class FuelLogMenuActivity extends AppCompatActivity {
+
+
+}
--- app/src/main/java/com/example/hello_android/MainVehicleMenuActivity.java
+++ app/src/main/java/com/example/hello_android/MainVehicleMenuActivity.java
@@ -1,6 +1,72 @@
 package com.example.hello_android;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainVehicleMenuActivity extends AppCompatActivity {
+    private Button fuelLogMenuButton;
+    private Button serviceLogMenuButton;
+    private Button receiptStorageButton;
+    private Button exportDataButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_vehicle_menu);
+
+        fuelLogMenuButton = this.findViewById(R.id.fuel_log_menu_button);
+        fuelLogMenuButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainVehicleMenuActivity.this, FuelLogMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        serviceLogMenuButton = this.findViewById(R.id.service_log_menu_button);
+        serviceLogMenuButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainVehicleMenuActivity.this, ServiceLogMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        receiptStorageButton = this.findViewById(R.id.receipt_storage_menu_button);
+        receiptStorageButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainVehicleMenuActivity.this, ReceiptStorageMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        exportDataButton = this.findViewById(R.id.export_data_menu_button);
+        exportDataButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainVehicleMenuActivity.this, MainVehicleMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
 }
