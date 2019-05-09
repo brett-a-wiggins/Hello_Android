47a33687b4ad9b62ea0d400bb621a1cecaf81dac
brett <brett@criticalanalysis.com.au>
2019-05-09 22:50:27 +1000

    Coding ExportDataMenuActivity. Working on writing data to file.

--- app/src/main/java/com/example/hello_android/ExportDataMenuActivity.java
+++ app/src/main/java/com/example/hello_android/ExportDataMenuActivity.java
@@ -8,27 +8,35 @@ import android.widget.Button;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class ExportDataMenuActivity extends AppCompatActivity {
-    Button export_button;
-    PrintWriter writer;
+    private Button export_button;
+    private PrintWriter writer;
+    private VehicleOwner current_owner;
+    private Vehicle current_vehicle;
+    private ArrayList<FuelTransaction> mFuelTransactions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export_data_menu);
+        export_button = findViewById(R.id.export_to_csv_button);
         export_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                current_vehicle = SelectVehicleActivity.getTempVehicle();
+                mFuelTransactions = current_vehicle.getFuelTransList();
+                try {
+                    writer = new PrintWriter(new File("guzzle.csv"));
+                    StringBuilder sb = new StringBuilder();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
-        try {
-            writer = new PrintWriter(new File("guzzle.csv"));
-            StringBuilder sb = new StringBuilder();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
     }
 
