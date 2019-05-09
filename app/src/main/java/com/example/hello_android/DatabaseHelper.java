d77646edaf5893d53437528b66980cbaf1fc358b
brett <brett@criticalanalysis.com.au>
2019-05-09 17:41:28 +1000

    Added class for Vehicle Database Schema

--- app/src/main/java/com/example/hello_android/DatabaseHelper.java
+++ app/src/main/java/com/example/hello_android/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME =  "logbook.db";
-    public static final String VEHICLE_TABLE = "vehicle_table";
+
 
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
