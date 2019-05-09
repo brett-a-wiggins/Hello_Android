7db0558df0aafe7857b5ada72e19751e37ff17bc
brett <brett@criticalanalysis.com.au>
2019-05-09 17:27:23 +1000

    Started work coding the SQLite database

---
+++ app/src/main/java/com/example/hello_android/DatabaseHelper.java
@@ -0,0 +1,26 @@
+package com.example.hello_android;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME =  "logbook.db";
+    public static final String VEHICLE_TABLE = "vehicle_table";
+
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
