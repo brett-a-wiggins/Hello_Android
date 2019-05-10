e1bf698b0139ce0732589e75fd1c8ed868cd3774
brett <brett@criticalanalysis.com.au>
2019-05-10 14:41:04 +1000

    Added Getters and Setters to ServiceTransaction

--- app/src/main/java/com/example/hello_android/ServiceTransaction.java
+++ app/src/main/java/com/example/hello_android/ServiceTransaction.java
@@ -23,6 +23,62 @@ public class ServiceTransaction extends Transaction {
        this.nextServiceDate = nextServiceDate;
    }
 
+    public double getLaborCost() {
+        return laborCost;
+    }
+
+    public void setLaborCost(double laborCost) {
+        this.laborCost = laborCost;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public double getPartCost() {
+        return partCost;
+    }
+
+    public void setPartCost(double partCost) {
+        this.partCost = partCost;
+    }
+
+    public int getNextServiceOdo() {
+        return nextServiceOdo;
+    }
+
+    public void setNextServiceOdo(int nextServiceOdo) {
+        this.nextServiceOdo = nextServiceOdo;
+    }
+
+    public Date getServiceDate() {
+        return serviceDate;
+    }
+
+    public void setServiceDate(Date serviceDate) {
+        this.serviceDate = serviceDate;
+    }
+
+    public Date getNextServiceDate() {
+        return nextServiceDate;
+    }
+
+    public void setNextServiceDate(Date nextServiceDate) {
+        this.nextServiceDate = nextServiceDate;
+    }
+
+    public String getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(String transaction) {
+        this.transaction = transaction;
+    }
+
     public String getServiceTransactionString(){
         this.transaction = "Service Date: " + this.serviceDate + ", Next Date: " + this.nextServiceDate + ", Labor Cost: $" + this.laborCost + ", Part Cost: " + this.partCost + ", Next Service Odometer: " + this.nextServiceOdo +  ", Total Cost: $" + (this.laborCost + this.partCost);
         return transaction;
