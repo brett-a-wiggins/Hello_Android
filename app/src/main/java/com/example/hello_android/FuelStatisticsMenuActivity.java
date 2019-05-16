2c1eb4931d6cb02052500b8133231a46a245d35f
brett <brett@criticalanalysis.com.au>
2019-05-16 16:21:21 +1000

    Cleaned up Statistics menu

--- app/src/main/java/com/example/hello_android/FuelStatisticsMenuActivity.java
+++ app/src/main/java/com/example/hello_android/FuelStatisticsMenuActivity.java
@@ -3,12 +3,18 @@ package com.example.hello_android;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class FuelStatisticsMenuActivity extends AppCompatActivity {
+
+    private Button viewByDate;
+    private Button viewByLocation;
+    private Button viewAll;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuel_statistics_menu);
 
     }
 }
